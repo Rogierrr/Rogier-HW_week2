9d016d319f57e41b83045779d517e50ff137e638
Rogier van Beers <rogierrr@hotmail.com>
2019-11-16 09:58:15 +0100

    assignment 3 including btn

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
-  function getInfo() {
-    fetch(`https://swapi.co/api/people/1`) 
-    .then(response => response.json()) 
-    .then(myJson => { 
-      document.body.innerHTML =
-        "<pre>" + JSON.stringify(myJson, null, " ") + "</pre>"
-    })
-      
-  }
+
+
+function getLuke() {
+  fetch(`https://swapi.co/api/people/1`) // make the request
+.then(response => response.json()) // convert response to json
+.then(myJson => { // display data in the browser
+  const random = document.getElementById ('random')
+  random.innerHTML =
+
+
+  // document.body.innerHTML =
+    "<pre>" + JSON.stringify(myJson, null, " ") + "</pre>"
+
+    console.log(myJson);
+})
+}
+
+function getVader() {
+  fetch(`https://swapi.co/api/people/4`) // make the request
+.then(response => response.json()) // convert response to json
+.then(myJson => { // display data in the browser
+  const darth = document.getElementById ('darth')
+  random.innerHTML =
+
+
+
+  // document.body.innerHTML =
+    "<pre>" + JSON.stringify(myJson, null, " ") + "</pre>"
+
+    console.log(myJson);
+})
+}
+
+
+
+
+
